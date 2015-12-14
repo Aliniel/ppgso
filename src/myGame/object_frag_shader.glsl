fd0b15d6062ff387272f77cfa865661d4f1ad31c
Branislav Makan <branislav.makan1994@gmail.com>
2015-12-14 08:24:15 +0100

    Game finished and ready.

--- src/myGame/object_frag_shader.glsl
+++ src/myGame/object_frag_shader.glsl
@@ -29,32 +29,50 @@ void main() {
   // Diffuse light
   vec4 diffuseColor = vec4(0,0,0,0);
 
-  vec3 lightDirection = normalize(mine1 - FragPos);
-  float dist = distance(mine1, FragPos);
-  float diff = max(dot(normal, vec4(lightDirection, 0.0f)), 0.0f);
-  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
-
-  lightDirection = normalize(mine2 - FragPos);
-  dist = distance(mine2, FragPos);
-  diff = max(dot(normal, vec4(lightDirection, 0.0f)), 0.0f);
-  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
-
-  lightDirection = normalize(mine3 - FragPos);
-  dist = distance(mine3, FragPos);
-  diff = max(dot(normal, vec4(lightDirection, 0.0f)), 0.0f);
-  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
-
-  lightDirection = normalize(lightPosition - FragPos);
-  dist = distance(lightPosition, FragPos);
-  diff = max(dot(normal, vec4(lightDirection, 0.0f)), 0.0f);
-  diffuseColor += 30.0f * diff * lightColor / pow(dist, 1.75f);
-
   // Speculiar light
+  vec4 specularColor = vec4(0,0,0,0);
   float specularStrength = 0.5f;
   vec3 viewDir = normalize(viewPosition - FragPos);
+
+// Diff Mine 1
+  vec3 lightDirection = normalize(mine1 - FragPos);
+  float dist = distance(mine1, FragPos);
+  float diff = max(dot(normal, vec4(lightDirection, 1.0f)), 0.0f);
+  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
+// Spec
   vec3 reflectDir = reflect(-lightDirection, vec3(normal));
-  float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 16);
-  vec4 specularColor = specularStrength * spec * lightColor;
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0f), 256);
+  specularColor += specularStrength * spec * lightColor;
+
+// Diff Mine 2
+  lightDirection = normalize(mine2 - FragPos);
+  dist = distance(mine2, FragPos);
+  diff = max(dot(normal, vec4(lightDirection, 1.0f)), 0.0f);
+  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
+// Spec
+  reflectDir = reflect(-lightDirection, vec3(normal));
+  spec = pow(max(dot(viewDir, reflectDir), 0.0f), 256);
+  specularColor += specularStrength * spec * lightColor;
+
+// Diff Mine 3
+  lightDirection = normalize(mine3 - FragPos);
+  dist = distance(mine3, FragPos);
+  diff = max(dot(normal, vec4(lightDirection, 1.0f)), 0.0f);
+  diffuseColor += 30.0f * diff * mineColor / pow(dist, 2.25f);
+// Spec
+  reflectDir = reflect(-lightDirection, vec3(normal));
+  spec = pow(max(dot(viewDir, reflectDir), 0.0f), 256);
+  specularColor += specularStrength * spec * lightColor;
+
+// Diff Player
+  lightDirection = normalize(lightPosition - FragPos);
+  dist = distance(lightPosition, FragPos);
+  diff = max(dot(normal, vec4(lightDirection, 1.0f)), 0.0f);
+  diffuseColor += 30.0f * diff * lightColor / pow(dist, 1.75f);
+// Spec
+  reflectDir = reflect(-lightDirection, vec3(normal));
+  spec = pow(max(dot(viewDir, reflectDir), 0.0f), 16);
+  specularColor += specularStrength * spec * lightColor;
 
   // Lookup the color in Texture on coordinates given by fragTexCoord
   FragmentColor = texture(Texture, FragTexCoord) * (diffuseColor + ambientColor + specularColor);
